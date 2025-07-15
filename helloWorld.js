4c7f4c986f6cf17d51dc2427144fd77511f280fe
LUCIFER ® <95967478+WiiZARDD@users.noreply.github.com>
2025-07-14 19:37:42 -0500

    Create helloWorld.js
    Very basic JavaScript example

---
+++ helloWorld.js
@@ -0,0 +1,32 @@
+/*
+Different ways to use strings
+1: ''
+2: ""
+3: ``
+*/
+
+console.log('Hello World!');
+console.log("Hello World!");
+console.log(`Hello World!`);
+
+//Split point
+console.log(` `);
+
+/*Different ways to log string outputs
+.toUpperCase (MAKES ALL CAPS)
+.toLowerCase (makes all lowercase)
+.trim (Removes      Any         Tab         Space)
+*/
+
+console.log('Hello World'.toUpperCase());
+console.log('Hello World'.toLowerCase());
+console.log('   Hello World    '.trim());
+
+//Split point
+console.log(` `);
+
+//Different ways to use string types
+
+console.log('Hello "World"'.toUpperCase());
+console.log("Hello World".toLowerCase());
+console.log(`   Hello's "World"\nNew Line!    `.trim());
